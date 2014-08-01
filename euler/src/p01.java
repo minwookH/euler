3f3037c241c196642bd3541e8dbf5696a0674646
HeoMinWook <pmpmopm1@naver.com>
2014-08-01 18:21:40 +0900

    synap_quiz

--- euler/src/p01.java
+++ euler/src/p01.java
@@ -1,8 +1,11 @@
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class p01 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int value = 1000;
+		/*int value = 1000;
 		int sum_result=0;
 		
 		for(int i = 1 ; i<=(value-1) / 3 ; i++){
@@ -11,6 +14,16 @@ public class p01 {
 		for(int i = 1 ; i<=(value-1) / 5 ; i++){
 			sum_result =  (5*i/3 == 0) ? 0 : sum_result+5*i;
 		}
-		System.out.println("sum result = "+sum_result);
+		System.out.println("sum result = "+sum_result);*/
+		
+		Map<String, String> a = new HashMap<String, String>();
+		
+		a.put("name", "aaa");
+		cc(a);
+		System.out.println(a.get("name"));
+	}
+	
+	public static void cc(Map<String, String> a){
+		a.put("name", "bbb");
 	}
 }
